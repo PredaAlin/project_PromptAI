da8bb113ec27881ad063582f6cd553b7e0a2afa0
PredaAlin <alinpreda0711@gmail.com>
2023-10-02 15:37:14 +0300

    seccond commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 export const metadata = {
-  title: "Prompttopia",
+  title: "PromptAI",
   description: "Discover ai prompts",
 };
 
